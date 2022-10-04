a1b163e2dd36f970a5d4c6331890ae96a29826a6
heydu <heydu@heydudeMacBook-Pro.local>
2022-10-04 11:29:14 +0800

    a sample which could use GET, i will add POST function later

---
+++ test_http/src/components/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button @click="getPosts">Post Lists</button>
+        <div v-for="post in posts" :key="post.id">
+            <h3>{{ post.id }}. {{ post.title }}</h3>
+            <p>{{ post.body }}</p>
+            <hr /> 
+        </div>
+    </div>
+</template>
+
+<script>
+//<hr />: seperate line
+import axios from 'axios'
+    export default {
+        name: "PostList",
+        //data is a function, return a list
+        data() {
+            return {
+                posts: [],
+            }
+        },
+        //methods is an objects
+        //getPosts() is a function from axios to fetch data using GET
+        methods: {
+            getPosts() {
+                axios.get('https://jsonplaceholder.typicode.com/posts')
+                .then((response) => {
+                    console.log(response.data)
+                    this.posts = response.data
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
