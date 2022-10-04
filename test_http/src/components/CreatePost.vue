762d29ae0cd53bca6e4cc9af1454e50116b8bf1d
heydu <976698800@qq.com>
2022-10-04 18:48:44 +0800

    link to temporary backend

--- test_http/src/components/CreatePost.vue
+++ test_http/src/components/CreatePost.vue
@@ -2,19 +2,20 @@
     <div>
         <form @submit.prevent="createPost">
             <div>
-                <label for="userId">Post User Id</label>
-                <input tyep="text" id="userId" v-model="formData.userId" />
+                <label for="userId">用户名：</label>
+                <input type="text" id="username" v-model="formData.username" />
             </div>
             <div>
-                <label for="title">Post Title</label>
-                <input tyep="text" id="title" v-model="formData.title" />
+                <label for="title">密码：</label>
+                <input type="text" id="password" v-model="formData.password" />
             </div>
-            <div>
-                <label for="body">Post Body</label>
-                <input tyep="text" id="body" v-model="formData.body" />
-            </div>
-            <button>Create Post</button>
+            <button @click="state = true">Login</button>
+            <button @click="state = false">Register</button>
         </form>
+        <h3 v-if="loginFailed">登录失败，请检查用户名或密码。</h3>
+        <h3 v-if="loginSuccess">登录成功</h3>
+        <h3 v-if="registerFailed">用户已存在</h3>
+        <h3 v-if="registerSuccess">注册成功</h3>
     </div>
 </template>
 
@@ -25,14 +26,44 @@ import axios from 'axios'
         data() {
             return {
                 formData: {
-                    userId: '',
-                    title: '',
-                    body: ''    
-                }
+                    username: '',
+                    password: ''
+                },
+                state: true,
+                loginFailed: false,
+                loginSuccess: false,
+                registerFailed: false,
+                registerSuccess: false
             }
         },
         methods: {
+          loginResponseHandler(response_data) {
+            if(response_data.code==-1){
+              this.loginFailed = true
+            }
+            else if(response_data.code==0){
+              this.loginSuccess = true
+            }
+          },
+          registerResponseHandler(response_data) {
+            if(response_data.code==-1){
+              this.registerFailed = true
+            }
+            else if(response_data.code==0){
+              this.registerSuccess = true
+            }
+          },
             createPost() {
+              if(this.state){
+                axios.post('https://mock.apifox.cn/m1/1695746-0-default/login', this.formData)
+                .then((response) => this.loginResponseHandler(response.data))
+                .catch((error) => console.log(error))
+              }
+              else{
+                axios.post('https://mock.apifox.cn/m1/1695746-0-default/register', this.formData)
+                .then((response) => this.registerResponseHandler(response.data))
+                .catch((error) => console.log(error))
+              }
                 axios.post('https://jsonplaceholder.typicode.com/posts', this.formData)
                 .then((response) => console.log(response))
                 .catch((error) => console.log(error))
--- test_http/src/components/PostList.vue
+++ test_http/src/components/PostList.vue
@@ -1,46 +1,47 @@
 <template>
-    <div>
-        <button @click="getPosts">Post Lists</button>
-        <h3 v-if="errorMsg">{{ errorMsg }}</h3>
-        <div v-for="post in posts" :key="post.id">
-            <h3>{{ post.id }}. {{ post.title }}</h3>
-            <p>{{ post.body }}</p>
-            <hr /> 
-        </div>
+  <div>
+    <button @click="getPosts">Post Lists</button>
+    <h3 v-if="errorMsg">{{ errorMsg }}</h3>
+    <div v-for="(news, index) in newsList" :key="index">
+      <a href="https://baidu.com">
+        <h3>{{ news.title }}</h3>
+      <p>{{ news.content }}</p>
+      </a>
+      <hr />
     </div>
+  </div>
 </template>
 
 <script>
 //<hr />: seperate line
-import axios from 'axios'
-    export default {
-        name: "PostList",
-        //data is a function, return a list
-        data() {
-            return {
-                posts: [],
-                errorMsg: ''
-            }
-        },
-        //methods is an objects
-        //getPosts() is a function from axios to fetch data using GET
-        methods: {
-            getPosts() {
-                //test http failed by changing the url following
-                axios.get('https://jsonplaceholder.typicode.com/posts')
-                .then((response) => {
-                    console.log(response.data)
-                    this.posts = response.data
-                })
-                .catch((error) => {
-                    console.log(error),
-                    this.errorMsg = 'Error retrieving data'
-                })
-            }
-        },
-    }
+import axios from "axios";
+export default {
+  name: "PostList",
+  //data is a function, return a list
+  data() {
+    return {
+      newsList: [],
+      errorMsg: "",
+    };
+  },
+  //methods is an objects
+  //getPosts() is a function from axios to fetch data using GET
+  methods: {
+    getPosts() {
+      //test http failed by changing the url following
+      axios
+        .get("https://mock.apifox.cn/m1/1695746-0-default/search")
+        .then((response) => {
+          console.log(response.data);
+          this.newsList = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error), (this.errorMsg = "Error retrieving data");
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
