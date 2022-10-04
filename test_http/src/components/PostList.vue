ef9d6f4c23ce8f30bb70db67936cb38b3976f1f4
heydu <heydu@heydudeMacBook-Pro.local>
2022-10-04 11:39:22 +0800

    add response to http failed

--- test_http/src/components/PostList.vue
+++ test_http/src/components/PostList.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <button @click="getPosts">Post Lists</button>
+        <h3 v-if="errorMsg">{{ errorMsg }}</h3>
         <div v-for="post in posts" :key="post.id">
             <h3>{{ post.id }}. {{ post.title }}</h3>
             <p>{{ post.body }}</p>
@@ -18,19 +19,22 @@ import axios from 'axios'
         data() {
             return {
                 posts: [],
+                errorMsg: ''
             }
         },
         //methods is an objects
         //getPosts() is a function from axios to fetch data using GET
         methods: {
             getPosts() {
+                //test http failed by changing the url following
                 axios.get('https://jsonplaceholder.typicode.com/posts')
                 .then((response) => {
                     console.log(response.data)
                     this.posts = response.data
                 })
                 .catch((error) => {
-                    console.log(error)
+                    console.log(error),
+                    this.errorMsg = 'Error retrieving data'
                 })
             }
         },
